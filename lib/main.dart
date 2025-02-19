c727b015fc3b59b3173a8285712df644134ab3b2
j1nwan <weamicron@gmail.com>
2025-02-19 21:17:22 +0900

    feat: 로케일 초기화 선언

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
 import 'app.dart';
 
-void main() {
-  runApp(
-    const ProviderScope(
-      child: MyApp(),
+void main() async {
+  initializeDateFormatting().then(
+    (value) => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
     ),
   );
 }
